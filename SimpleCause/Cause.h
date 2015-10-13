1c3d11ee28e292345f5079b5d8011b8ab1c16bf0
João Alves <joao.cpp.sca@gmail.com>
2015-10-13 19:28:00 +0100

    Initial Commit

---
+++ SimpleCause/Cause.h
@@ -0,0 +1,13 @@
+//
+//  Cause.h
+//  SimpleCause
+//
+//  Created by João Alves on 10/13/15.
+//  Copyright © 2015 Learn. All rights reserved.
+//
+
+#ifndef Cause_h
+#define Cause_h
+
+
+#endif /* Cause_h */
---
+++ SimpleCause/Cause.m
@@ -0,0 +1,9 @@
+//
+//  Cause.m
+//  SimpleCause
+//
+//  Created by João Alves on 10/13/15.
+//  Copyright © 2015 Learn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
